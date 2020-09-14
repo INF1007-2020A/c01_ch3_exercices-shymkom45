a8b2783cea0a7e6518a1fa68d83bf66ac0ed1f44
shymkom45 <maximus.shymko@polymtl.ca>
2020-09-14 17:33:52 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -13,7 +13,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
     minutes = angle_mins / 60
     secondes = angle_secs / 3600
     
-    return math.radians(ang)
+    return math.radians(angle_degs + minutes + secondes)
 
 
 def to_degrees(angle_rads: float) -> tuple:
