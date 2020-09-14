a98d2fb4bfd84c6a0d95c847868d911a667065d2
shymkom45 <maximus.shymko@polymtl.ca>
2020-09-14 17:33:16 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -12,6 +12,7 @@ def average(a: float, b: float, c: float) -> float:
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
     minutes = angle_mins / 60
     secondes = angle_secs / 3600
+    math.
     return 
 
 
